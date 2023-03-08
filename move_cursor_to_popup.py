ac1f34a956abb61a85427a070130f90789101875
bkitano <briankitano@gmail.com>
2023-03-08 15:21:19 -0500

    move cursor to popup works

---
+++ move_cursor_to_popup.py
@@ -0,0 +1,30 @@
+import pyautogui
+import library
+import webbrowser 
+from time import sleep
+from PIL import Image
+
+# two stills: last frame of first video and first frame of last video
+# open both frames in chrome
+# get mouse position in second frame, then animate mouse movement
+
+# get first frame of last gif
+FIRST_FRAME_PATH = './first_frame.png'
+CURSOR_PATH = './cursor2.png'
+
+CHROME_PATH = 'open -a /Applications/Google\ Chrome.app %s'
+SCREEN_SIZE = tuple(pyautogui.size())
+
+browser = webbrowser.get(CHROME_PATH)
+browser.open(FIRST_FRAME_PATH, 1)
+
+sleep(1)
+screencap = pyautogui.screenshot().convert('RGB')
+screencap.paste(Image.new(size=(int(screencap.size[0]/2 - 400), int(screencap.size[1])),mode=screencap.mode), (0,0))
+screencap.paste(Image.new(size=(int(screencap.size[0]/2 - 400), int(screencap.size[1])),mode=screencap.mode), (int(screencap.size[0]/2 + 400),0))
+screencap.paste(Image.new(size=(int(screencap.size[0]), int(screencap.size[1]/2 - 400)),mode=screencap.mode), (0,0))
+screencap.paste(Image.new(size=(int(screencap.size[0]), int(screencap.size[1]/2 - 400)),mode=screencap.mode), (0,int(screencap.size[1]/2 + 400)))
+
+final_cursor_position = library.get_button_coordinates(screencap, CURSOR_PATH, SCREEN_SIZE)
+
+pyautogui.moveTo(*final_cursor_position, duration=1)
