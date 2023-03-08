07076f91ee94808e33b8a6e41a2c060f99894d93
bkitano <briankitano@gmail.com>
2023-03-08 13:07:13 -0500

    superimposing clip on dark mock video

--- impose_video.py
+++ impose_video.py
@@ -1,12 +1,14 @@
 from PIL import Image
 import library
 from moviepy.editor import VideoFileClip, CompositeVideoClip
+import cv2
 
 import os
 os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
 
 MOCK_PATH = '/Users/bkitano/Desktop/projects/upstream/Slice-1.png'
 DARK_MOCK_FILE_PATH = './dark.png'
+DARK_VIDEO_PATH = './dark_video'
 
 mock_img = Image.open(MOCK_PATH)
 dark_screen = library.darken_image(mock_img)
@@ -14,10 +16,34 @@ dark_screen.save(DARK_MOCK_FILE_PATH, 'PNG')
 
 # record video of dark screen, and impose rodrigo's screencap
 rodrigo_clip = VideoFileClip('./popup.mp4', audio=False)
+clip_height, clip_width = rodrigo_clip.size
 
 # make the blur video
-library.make_video_from_frame(DARK_MOCK_FILE_PATH, rodrigo_clip.duration, './dark_video')
+library.make_video_from_frame(DARK_MOCK_FILE_PATH, rodrigo_clip.duration, DARK_VIDEO_PATH)
+dark_clip = VideoFileClip(DARK_VIDEO_PATH + '.avi')
 
-# video = CompositeVideoClip([
-#     rodrigo_clip.set_position()
-# ])
+img = cv2.imread(DARK_MOCK_FILE_PATH)
+mock_height, mock_width, layers = img.shape
+
+print("mock dimensions")
+print( mock_height)
+print( mock_width)
+
+print("clip dimensions")
+print( clip_height)
+print( clip_width)
+
+clip_position = (
+    int(mock_width/2. - clip_width / 2.),
+    int( mock_height/2. - clip_height / 2.)
+)
+print(clip_position)
+
+video = CompositeVideoClip([
+    dark_clip,
+    rodrigo_clip.set_position(clip_position),
+],
+    size=(mock_width, mock_height)
+)
+
+video.write_gif('./test.gif', fps=10)
