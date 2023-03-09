19371498f4b862691d8291bc56ff13881e4c3cfa
bkitano <briankitano@gmail.com>
2023-03-09 02:00:01 -0500

    i think finished

--- exec.sh
+++ exec.sh
@@ -1,7 +1,8 @@
 # ./exec.sh /Users/bkitano/Desktop/projects/upstream/mocker/slice.png 
 
 python3 recorder.py ./outputs/opener.avi 30 2 0 & python3 mocker.py $1
-python3 impose_video.py /Users/bkitano/Desktop/projects/upstream/mocker/slice.png ./outputs/imposer.mp4
+python3 impose_video.py $1 ./outputs/imposer.mp4
 python3 recorder.py ./outputs/mover.avi 10 1 10 & python3 move_cursor_to_popup.py 
 sleep 15
-python3 stitch.py ./outputs/opener.avi ./outputs/mover.avi ./outputs/imposer.mp4 ./outputs/final.gif
+python3 stitch.py ./outputs/opener.avi ./outputs/mover.avi ./outputs/imposer.mp4 ./final.gif
+rm -f ./outputs/*
