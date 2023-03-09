0d4ffcbb9044ab45492ae9ed2f5c920b3eceb981
bkitano <briankitano@gmail.com>
2023-03-08 23:53:29 -0500

    fixes one thing, breaks another, haha

--- impose_video.py
+++ impose_video.py
@@ -3,19 +3,30 @@ import library
 from moviepy.editor import VideoFileClip, CompositeVideoClip
 import cv2
 import sys
-
+import webbrowser
+import pyautogui
 import os
+from time import sleep
 os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
 
 MOCK_PATH = sys.argv[1]
 IMPOSER_PATH = sys.argv[2]
 DARK_MOCK_FILE_PATH = './dark.png'
+DARK_MOCK_SCREENCAP_PATH = './dark_cap.png'
 DARK_VIDEO_PATH = './dark_video.avi'
+CHROME_PATH = 'open -a /Applications/Google\ Chrome.app %s'
 
 mock_img = Image.open(MOCK_PATH)
 dark_screen = library.darken_image(mock_img)
 # need to superimpose this image on the chrome window
+
 dark_screen.save(DARK_MOCK_FILE_PATH, 'PNG')
+
+browser = webbrowser.get(CHROME_PATH)
+browser.open(DARK_MOCK_FILE_PATH, 1)
+
+sleep(1)
+screencap = pyautogui.screenshot(DARK_MOCK_SCREENCAP_PATH)
 
 # record video of dark screen, and impose rodrigo's screencap
 rodrigo_clip = VideoFileClip('./popup.mp4', audio=False)
@@ -23,10 +34,10 @@ rodrigo_clip = rodrigo_clip.resize(1.6)
 clip_height, clip_width = rodrigo_clip.size
 
 # make the dark video
-library.make_video_from_frame(DARK_MOCK_FILE_PATH, rodrigo_clip.duration, DARK_VIDEO_PATH)
+library.make_video_from_frame(DARK_MOCK_SCREENCAP_PATH, rodrigo_clip.duration, DARK_VIDEO_PATH)
 dark_clip = VideoFileClip(DARK_VIDEO_PATH)
 
-img = cv2.imread(DARK_MOCK_FILE_PATH)
+img = cv2.imread(DARK_MOCK_SCREENCAP_PATH)
 mock_height, mock_width, layers = img.shape
 
 clip_position = (
