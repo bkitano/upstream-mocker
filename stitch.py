5da8a30f57ee979f0a9e5852a1bd740b24b3c61d
bkitano <briankitano@gmail.com>
2023-03-15 16:29:31 -0400

    fixing bug where missing args

--- stitch.py
+++ stitch.py
@@ -3,21 +3,12 @@ from moviepy.video.fx.all import crop
 import library
 import sys
 
-opener_clip = VideoFileClip(sys.argv[1])
-mover_clip = VideoFileClip(sys.argv[2])
-imposer_clip = VideoFileClip(sys.argv[3])
+clips = [ VideoFileClip(sys.argv[i]) for i in range(1, len(sys.argv) - 1) ]
+print([clip.size for clip in clips])
 
-print("opener", opener_clip.size)
-print("mover", mover_clip.size)
-print("imposer", imposer_clip.size)
-
-final_clip = concatenate_videoclips([
-    opener_clip,
-    mover_clip,
-    imposer_clip
-])
+final_clip = concatenate_videoclips(clips)
 
 cropped = crop(final_clip, x1=0, y1=library.CHROME_AND_TOP_NAV_MARGIN,
-               x2=final_clip.size[0], y2=final_clip.size[1]-library.DOCK_MARGIN)
+            x2=final_clip.size[0], y2=final_clip.size[1]-library.DOCK_MARGIN)
 
-cropped.write_gif(sys.argv[4], fps=8)
+cropped.write_gif(sys.argv[-1], fps=8)
