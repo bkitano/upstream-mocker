424157535f0746b49ec9ea44dbff244cf78d906b
bkitano <briankitano@gmail.com>
2023-03-15 05:26:43 -0400

    fixed weird sizing, now need to fix resizing of popup window

--- login.sh
+++ login.sh
@@ -1,12 +1,22 @@
-# ./login.sh Amota\ Group logo.jpeg slice.png
+# ./login.sh Amota\ Group /Users/bkitano/Desktop/projects/upstream/mocker/logo.jpeg /Users/bkitano/Desktop/projects/upstream/mocker/slice.png test.gif
+# have to be absolute paths
 
 # 1 - company name
 # 2 - logo
 # 3 - mock login scren
+# 4 - output file
 
-echo "$1, $2, $3"
+echo "$1, $2, $3, $4"
 
 python3 login_fill_popover.py "$2" "$1" ./outputs/filled_popover.png
 python3 login_create_dark_mask.py "$3" ./outputs/filled_popover.png
+python3 login_overlay_filled_popover_on_dark_mock.py /Users/bkitano/Desktop/projects/upstream/mocker/outputs/dark.png
 
+# recording section
+python3 recorder.py ./outputs/login_opener.avi 30 2 0 & python3 mocker.py $3 
+python3 recorder.py ./outputs/login_move_cursor_to_popup.avi 10 1 10 & python3 login_move_cursor_to_popup.py
+python3 login_move_cursor_to_popup.py
+sleep 5
+
+python3 stitch.py ./outputs/login_opener.avi ./outputs/login_move_cursor_to_popup.avi ./outputs/mock_with_popover.mp4 ./test.gif
 # rm -rf ./outputs/*
--- login_overlay_filled_popover_on_dark_mock.py
+++ login_overlay_filled_popover_on_dark_mock.py
@@ -1,19 +1,37 @@
 from PIL import Image
 from moviepy.editor import VideoFileClip, CompositeVideoClip, ImageClip
+import webbrowser
+import sys
+import pyautogui
+import library
+from time import sleep
+import numpy as np
 
 # OVERLAY_VIDEO_PATH = './outputs/filled_popover.avi' # use this for render
 POPOVER_VIDEO_PATH = './outputs/filled_popover.mp4'  # use this for debug
-DARK_MOCK_PATH = './outputs/dark.png'
+DARK_MOCK_PATH = sys.argv[1]
 
-
-mock_img = Image.open(DARK_MOCK_PATH)
+# screencap chrome
+browser = webbrowser.get(library.CHROME_PATH)
+browser.open(DARK_MOCK_PATH, 1)
+sleep(1)
+mock_in_chrome_screencap = pyautogui.screenshot()
 
 popover_clip = VideoFileClip(POPOVER_VIDEO_PATH).set_position(("center", "center"))
 
 # dark video
-dark_clip = ImageClip(DARK_MOCK_PATH).set_duration(popover_clip.duration)
+dark_clip = ImageClip(np.array(mock_in_chrome_screencap.convert("RGBA"))).set_duration(popover_clip.duration)
 
-clip = CompositeVideoClip([dark_clip, popover_clip], size=mock_img.size)
+clip = CompositeVideoClip([dark_clip, popover_clip], size=mock_in_chrome_screencap.size)
 
 # clip.write_videofile('./outputs/mock_with_popover.avi', fps=10, codec='png') # use for render
 clip.write_videofile('./outputs/mock_with_popover.mp4', fps=10, codec='mpeg4') # use for debug
+
+# first frame has to be the non-chrome
+del dark_clip
+del clip 
+
+mock_img = Image.open(DARK_MOCK_PATH)
+dark_clip = ImageClip(DARK_MOCK_PATH).set_duration(.1)
+clip = CompositeVideoClip([dark_clip, popover_clip], size=mock_img.size)
+clip.save_frame('./outputs/login_first_frame.png')
