9bafc70ee9b9cedf7264e48bef8e0188edcca853
bkitano <briankitano@gmail.com>
2023-03-15 03:49:10 -0400

    creating a reusable text box

--- login_fill_popover.py
+++ login_fill_popover.py
@@ -2,6 +2,8 @@ from PIL import Image, ImageFont, ImageDraw
 import sys
 import textwrap
 
+from moviepy.editor import VideoFileClip, CompositeVideoClip, ImageClip
+
 # fill_popover.py logo.jpeg Amota\ Group ./outputs/filled_popover.png
 
 BLANK_POPOVER_IMAGE_PATH = './assets/blank_popover.png'
@@ -19,7 +21,7 @@ LOGO_PATH = sys.argv[1]
 BUSINESS_NAME = sys.argv[2]
 FILLED_POPOVER_IMAGE_PATH = sys.argv[3]
 
-blank_popover = Image.open(BLANK_POPOVER_IMAGE_PATH)
+blank_popover = Image.open(BLANK_POPOVER_IMAGE_PATH).convert('RGBA')
 logo = Image.open(LOGO_PATH)
 logo = logo.resize((box_height, box_width))
 logo_position = (int(BOX_CENTER[0] - box_width/2.),
@@ -31,7 +33,9 @@ blank_popover.paste(logo, logo_position)
 other_text = f"""\
 {BUSINESS_NAME} will receive the following: your public business details, trade references, primary email address, and credit profile."""
 
-draw_image = ImageDraw.Draw(blank_popover)
+# draw_image = ImageDraw.Draw(blank_popover)
+text_img = Image.new('RGBA', blank_popover.size)
+draw = ImageDraw.Draw(text_img, 'RGBA')
 regular_font = ImageFont.truetype(FONT_PATH, FONT_SIZE)
 
 text_box_position = (TEXT_CENTER[0],
@@ -51,10 +55,16 @@ for line in lines:
         y_text
     )
 
-    draw_image.text(line_position, line,
+    draw.text(line_position, line,
                     font=regular_font, fill="black", align="center")
     y_text += (line_height + LINE_SPACING)
 
+blank_popover.paste(text_img, (0,0), text_img)
+blank_popover.show()
 blank_popover.save(FILLED_POPOVER_IMAGE_PATH)
 
 # overlay logo, text file on top of blank popover video
+# blank_popover_clip = VideoFileClip('./assets/blank_login_popover.mov')
+# blank_popover_clip = blank_popover_clip.resize(blank_popover.size)
+
+# logo_clip = ImageClip(logo).set_position(logo_position)
